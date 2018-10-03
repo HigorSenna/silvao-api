7a2681ae959fdf911deb46bfd3258dc671a21235
Higor Senna <higorr_jf@hotmail.com>
2018-10-03 00:26:22 -0300

    Adicionado script do banco de dados

---
+++ banco/silvao_roll_db.sql
@@ -0,0 +1,78 @@
+CREATE DATABASE  IF NOT EXISTS `silvao_roll` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `silvao_roll`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Linux (x86_64)
+--
+-- Host: localhost    Database: silvao_roll
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.1.16-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `maletas`
+--
+
+DROP TABLE IF EXISTS `maletas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `maletas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `caminho_imagem` varchar(80) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=56 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `maletas`
+--
+
+LOCK TABLES `maletas` WRITE;
+/*!40000 ALTER TABLE `maletas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `maletas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `premios`
+--
+
+DROP TABLE IF EXISTS `premios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `premios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_maleta` int(11) DEFAULT NULL,
+  `descricao` varchar(70) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_premios_1_idx` (`id_maleta`),
+  CONSTRAINT `fk_premios_1` FOREIGN KEY (`id_maleta`) REFERENCES `maletas` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `premios`
+--
+
+LOCK TABLES `premios` WRITE;
+/*!40000 ALTER TABLE `premios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `premios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-10-03  0:25:50
